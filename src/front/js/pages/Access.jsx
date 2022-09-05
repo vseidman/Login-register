46c51a91f125a18c89711280a57a97f092420e9c
Victor Seidman <victorseidman@gmail.com>
2022-09-05 19:26:02 -0300

    Proyecto listo

--- src/front/js/pages/Access.jsx
+++ src/front/js/pages/Access.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import skywalker from "../../img/luke.jpg"
 
 const Access = () => {
 
   return (
     <>
+      <>
+        <div className="">
+          <div className="card my-3 card-css">
+            <img src={skywalker} className="card-img-top logo-luke" />
+            <div className="card-body">
+              <h5 className="card-title ">Luke Skywalker</h5>
+              <div className="card-footer">
+                <ul>
+                  <li>Eye color: Blue</li>
+                  <li>Hair color: Blonde</li>
+                  <li>Lightsaber: Green</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
     </>
   );
 };
